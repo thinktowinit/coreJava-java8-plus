bf3805f2a62a18a3d4eeff2a0364bec96cc3d759
govardhan reddy <govardahn@gmail.com>
2025-07-22 22:18:07 +0530

    added package

--- eclipse-workspace-ravi-ksp/springboot/Core-Java/src/com/employee/Employee.java
+++ eclipse-workspace-ravi-ksp/springboot/Core-Java/src/com/employee/Employee.java
@@ -14,79 +14,62 @@ public class Employee {
 	private int bloodGroupPrice;
 	private int age;
 	private List<String> frindNames;
-	
-	
-
-	public List<String> getFrindNames() {
-		return frindNames;
-	}
-
-	public void setFrindNames(List<String> frindNames) {
-		this.frindNames = frindNames;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getSalary() {
 		return salary;
 	}
-
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
-	public void setGender(String gender) {
+	public void setGender(String gender) {  
 		this.gender = gender;
 	}
-
 	public boolean isAdmin() {
 		return isAdmin;
 	}
-
 	public void setAdmin(boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
-
 	public String getBloodGroup() {
 		return bloodGroup;
 	}
-
 	public void setBloodGroup(String bloodGroup) {
 		this.bloodGroup = bloodGroup;
 	}
-
 	public int getBloodGroupPrice() {
 		return bloodGroupPrice;
 	}
-
 	public void setBloodGroupPrice(int bloodGroupPrice) {
 		this.bloodGroupPrice = bloodGroupPrice;
 	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public List<String> getFrindNames() {
+		return frindNames;
+	}
+	public void setFrindNames(List<String> frindNames) {
+		this.frindNames = frindNames;
+	}
+	
 
+	
+	
 }
